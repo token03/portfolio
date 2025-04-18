7e80ee5fbbdc79d335db9dec8337e21b4180d687
Jessie Zhong <zhongjessie2003@gmail.com>
2025-04-18 04:19:12 -0700

    asdadsa

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,26 @@ function App() {
     <div className="main-container">
       <section className="about-me">
         <h3 className="left-text small-margin">hi im</h3>
-        <h1 className="left-text underlined-title">jessie zhong</h1>
-        <h3 className="left-text top-margin">third year cs @ ubc</h3>
+        <h1 className="left-text small-margin">jessie zhong</h1>
+        <h3 className="left-text small-margin">third year cs @ ubc</h3>
       </section>
-      <section className="projects">
-        <h2 className="right-text underlined-title">projects</h2>
-        <div className="project"></div>
-        <div className="project"></div>
-        <div className="project"></div>
+
+      <hr className="separator" />
+
+      <section className="projects left-text top-margin">
+        <h2 className="underlined-title">projects</h2>
+        <h3 className="small-margin">card collector app</h3>
+        <h3 className="small-margin">rhythm game classifier</h3>
+        <h3 className="small-margin">ml manga translator</h3>
+        <h3 className="small-margin">pokemon discord bot</h3>
       </section>
-      <section className="sections">
-        <h2 className="right-text underlined-title">experience</h2>
-        <div className="emonster"></div>
-        <div className="mastercard"></div>
-        <div className="mastercard"></div>
+
+      <hr className="separator top-margin" />
+
+      <section className="sections left-text top-margin">
+        <h2 className="underlined-title">experience</h2>
+        <h3 className="small-margin">emonster solutions</h3>
+        <h3 className="small-margin">mastercard</h3>
       </section>
     </div>
   );
